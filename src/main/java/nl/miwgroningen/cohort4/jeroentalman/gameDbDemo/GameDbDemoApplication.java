9e4d45df05b1cf1cd5e83da55d950c433bb4886b
Jeroen Talman <jeroentalman@gmail.com>
2020-11-18 09:58:01 +0100

    Empty projectmap

---
+++ src/main/java/nl/miwgroningen/cohort4/jeroentalman/gameDbDemo/GameDbDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.cohort4.jeroentalman.gameDbDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameDbDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwgroningen/cohort4/jeroentalman/gameDbDemo/GameDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.cohort4.jeroentalman.gameDbDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
